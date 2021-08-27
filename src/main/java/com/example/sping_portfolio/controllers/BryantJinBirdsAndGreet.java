66ec813214bf1a0ac72b9227d2a5ba91f4042ce4
HexaDrakon <bryantjinofficial@gmail.com>
2021-08-27 09:51:39 -0700

    Routing hotfix
    Change route names to try and stop the errors

--- src/main/java/com/example/sping_portfolio/controllers/BryantJinBirdsAndGreet.java
+++ src/main/java/com/example/sping_portfolio/controllers/BryantJinBirdsAndGreet.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class BryantJinBirdsAndGreet {
-    /*@GetMapping("/birds")
+    @GetMapping("/birds2")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String birds() {
         return "birds"; // returns HTML VIEW (greeting)
     }
 
-    @GetMapping("/greet")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    @GetMapping("/greet2")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
         return "greet"; // returns HTML VIEW (greeting)
-    }*/
+    }
 }
 
