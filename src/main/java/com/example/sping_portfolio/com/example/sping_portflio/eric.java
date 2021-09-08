3c3fd7a0b4282f452ea28e360300daaeb3d9561f
ericl <ericliwang55555@gmail.com>
2021-09-08 10:19:50 -0700

    eric test

---
+++ src/main/java/com/example/sping_portfolio/com/example/sping_portflio/eric.java
@@ -0,0 +1,12 @@
+package com.example.sping_portfolio.com.example.sping_portflio;
+
+import java.util.Scanner;
+
+public class Eric {
+
+    public static void main(String[] args) {
+
+        System.out.print("Eric is cool");
+
+    }
+}
