1673257472085c9f71887da3be4fe0d9ca0744c3
CalrethonOfMirkwood <82109882+CalrethonOfMirkwood@users.noreply.github.com>
2023-02-05 20:56:54 -0800

    cors test

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -21,9 +21,11 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/volumes/uploads/**").addResourceLocations("file:volumes/uploads/");
     }
 
+    /*
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*");
     }
+    */
 
 }
