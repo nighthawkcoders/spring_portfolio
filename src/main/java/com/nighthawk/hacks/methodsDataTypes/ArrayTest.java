dff0372ab588d2e9d436127b7ee27a6bdaecbdb3
jm1021 <jmortensen@powayusd.com>
2024-03-28 16:07:34 -0700

    fix typos

--- src/main/java/com/nighthawk/hacks/methodsDataTypes/ArrayTest.java
+++ src/main/java/com/nighthawk/hacks/methodsDataTypes/ArrayTest.java
@@ -8,11 +8,11 @@ public class ArrayTest {
     public static void changeArray(String[] aLocalRef, ArrayTest aObject) {
         System.out.println("In changeArray method");
         System.out.println("\t changeArray method before: aLocalRef = " + Arrays.toString(aLocalRef) + ", hash = " + System.identityHashCode(aLocalRef));
-        System.out.println("\t changeArray method before: sObject.s = " + Arrays.toString(aObject.arr) + ", hash = " + System.identityHashCode(aObject));
+        System.out.println("\t changeArray method before: aObject = " + Arrays.toString(aObject.arr) + ", hash = " + System.identityHashCode(aObject));
         aLocalRef[2] = "ghi";  // allowed as content in arrays is mutable, array is passed by reference 
         aObject.arr[2] = "ghi";
         System.out.println("\t changeArray method after: aLocalRef = " + Arrays.toString(aLocalRef) + ", hash = " + System.identityHashCode(aLocalRef));
-        System.out.println("\t changeArray method after: aObject.s = " + Arrays.toString(aObject.arr) + ", hash = " + System.identityHashCode(aObject));
+        System.out.println("\t changeArray method after: aObject = " + Arrays.toString(aObject.arr) + ", hash = " + System.identityHashCode(aObject));
     }
 
     public static void main(String[] args) {
@@ -27,12 +27,12 @@ public class ArrayTest {
         // ArrayTest object is a new object that is created on the heap
         ArrayTest aObject = new ArrayTest();
         
-        System.out.println("main method before: sValue = " + Arrays.toString(arr)  + ", hash = " + System.identityHashCode(arr));
-        System.out.println("main method before: sObject.s = " + Arrays.toString(aObject.arr) + ", hash = " + System.identityHashCode(aObject));
+        System.out.println("main method before:  arr= " + Arrays.toString(arr)  + ", hash = " + System.identityHashCode(arr));
+        System.out.println("main method before: aObject = " + Arrays.toString(aObject.arr) + ", hash = " + System.identityHashCode(aObject));
     
         changeArray(arr, aObject); // arr is heap value, aObject is heap reference to ArrayTest object 
         
-        System.out.println("main method before: sValue = " + Arrays.toString(arr) + ", hash = " + System.identityHashCode(arr));
-        System.out.println("main method before: sObject.s = " + Arrays.toString(aObject.arr) + ", hash = " + System.identityHashCode(aObject));
+        System.out.println("main method before: arr = " + Arrays.toString(arr) + ", hash = " + System.identityHashCode(arr));
+        System.out.println("main method before: aObject = " + Arrays.toString(aObject.arr) + ", hash = " + System.identityHashCode(aObject));
     }
 }
