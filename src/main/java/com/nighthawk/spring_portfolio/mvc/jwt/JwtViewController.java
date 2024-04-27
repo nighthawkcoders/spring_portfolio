c3662c86ded552bec85b77fd0a17ea92dd6068e4
jm1021 <jmortensen@powayusd.com>
2024-04-27 15:18:40 -0700

    add CrossOrigin back

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtViewController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtViewController.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import com.nighthawk.spring_portfolio.mvc.person.Person;
 import com.nighthawk.spring_portfolio.mvc.person.PersonDetailsService;
 
 @Controller
+@CrossOrigin
 public class JwtViewController {
 
 	@Autowired
