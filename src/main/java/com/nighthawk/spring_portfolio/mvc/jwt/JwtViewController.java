f2903661fa6711c48cd57ffc40424bdfcb3fc617
jm1021 <jmortensen@powayusd.com>
2024-04-28 05:22:01 -0700

    test alternate endpoint

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtViewController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtViewController.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +24,6 @@ import com.nighthawk.spring_portfolio.mvc.person.Person;
 import com.nighthawk.spring_portfolio.mvc.person.PersonDetailsService;
 
 @Controller
-@CrossOrigin
 public class JwtViewController {
 
 	@Autowired
